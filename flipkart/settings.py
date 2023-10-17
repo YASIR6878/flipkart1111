0e772bdaede514defcf630d60a23297cd9ca5a93
YASIR6878 <121033130+YASIR6878@users.noreply.github.com>
2023-10-18 00:44:29 +0530

    changed

--- flipkart/settings.py
+++ flipkart/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-70zl(kb4299ra3d!2$ef4437vogu81&fs4q7h-)&af0-s899k)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','.now.sh','localhost']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 '''
 ALLOWED_HOSTS = []
 '''
